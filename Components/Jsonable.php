014d8787a06a53cfbd989997fa848ffc7df2ba80
Luther King <massbevbp@gmail.com>
2017-11-30 18:02:20 +0200

    code review #5

--- Components/Jsonable.php
+++ Components/Jsonable.php
@@ -6,18 +6,20 @@ abstract class Jsonable
 {
     public function __toString()
     {
-        return json_encode($this->arrayToJson);
+        return json_encode($this->toArray());
     }
 
     public function toArray()
     {
+        $arrayToJson = [];
+
         $objectReflection = new \ReflectionClass($this);
 
         foreach ($objectReflection->getProperties(\ReflectionProperty::IS_PUBLIC) as $item){
             if(is_int($item->getValue($this)) || is_string($item->getValue($this)) || is_array($item->getValue($this))){
-                $this->arrayToJson[$item->getName()] = $item->getValue($this);
+                $arrayToJson[$item->getName()] = $item->getValue($this);
             }
         }
-        return $this->arrayToJson;
+        return $arrayToJson;
     }
 }
--- index.php
+++ index.php
@@ -6,6 +6,5 @@ use Components\JsonableExperiment;
 
 $object = new JsonableExperiment(23, 'Vikaaaa', array(1 => 10, 2 => 30, 'dssdfsd' => 23423), true);
 $object->setPrivateField('Приватное поле');
-$object->toArray();
 
 echo Controller::action($object);
