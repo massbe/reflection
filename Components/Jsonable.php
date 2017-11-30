126cd6d54b827d16d4ba641deb87465e05f74181
Luther King <massbevbp@gmail.com>
2017-11-30 15:44:59 +0200

    code review #2

--- Components/Jsonable.php
+++ Components/Jsonable.php
@@ -9,18 +9,25 @@ class Jsonable
     public $id;
     public $name;
     public $fieldLOL;
+    public $boolVar;
     private $privateField;
+    private $allObject;
 
-    public function __construct($id, $name, $fieldLOL)
+    public function __construct($id, $name, $fieldLOL, $boolVar)
     {
         $this->id = $id;
         $this->name = $name;
         $this->fieldLOL = $fieldLOL;
+        $this->boolVar = $boolVar;
     }
 
     public function __toString()
     {
-        return $this->id;
+        return $this->allObject;
+    }
+
+    public function setAllObject($value){
+        $this->allObject = $value;
     }
 
     public function getPrivateField()
--- index.php
+++ index.php
@@ -4,92 +4,33 @@ include_once 'vendor/autoload.php';
 use Components\Controller;
 use Components\Jsonable;
 
-$object = new Jsonable(23, 'Vika', 'KEK');
-$object->setPrivateField('Приватное поле');
-echo $object;
-
-$objectReflection = new ReflectionClass($object);
+//Переменная, в которую я записываю значения объекта Jsonable после сверки их типов
 $arrayToJson = [];
 
+/*
+Здесь я задаю значения объекту Jsonable. Я создал для него специальный конструктор, который записывает
+данные в разные поля объекта (public, private, protected). Чтобы проверить работоспособность кода
+*/
+
+$object = new Jsonable(23, 'Vikaaaa', array(1 => 10, 2 => 30, 'dssdfsd' => 23423), true);
+$object->setPrivateField('Приватное поле');
+
+//Вызываю класс Рефлексии, чтобы получить данные из созданного объекта типа Jsonable
+$objectReflection = new ReflectionClass($object);
+
+//Здесь я с помощью рефлексии перебираю каждый элемент свойства объекта и сверяю их типы. Нам надо: int, string и array
 foreach ($objectReflection->getProperties(ReflectionProperty::IS_PUBLIC) as $item){
-    if(is_int($item->getValue($object)) || is_string($item->getValue($object))){
+    if(is_int($item->getValue($object)) || is_string($item->getValue($object)) || is_array($item->getValue($object))){
         $arrayToJson[$item->getName()] = $item->getValue($object);
-    }
-    elseif (is_array($item->getValue($object))){
-        echo json_decode($item->getValue($object));
     }
 }
 
-//var_dump(Controller::action($object));
-
-var_dump(Controller::action($object));
-//foreach ($objectReflection->getProperties() as $element){
-
-//}
+//Добавляю в класс значение
+$object->setAllObject(json_encode($arrayToJson));
 
 
+//Что нужно было вызвать. Как выдно, нет типов, который не подходят условиям (пустых, float)
+echo Controller::action($object);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$objectReflection = new ReflectionClass($object);
-$a = $objectReflection->getProperties(ReflectionProperty::IS_PUBLIC);
-
-foreach ($a as $item){
-    $testWhatType = $item->getValue($object);
-    if(is_string($testWhatType) || is_int($testWhatType)){
-        return $testWhatType;
-    }
-    elseif (is_array($testWhatType)){
-        return json_decode($testWhatType);
-    }
-    var_dump($testWhatType);
-}*/
+//$methodReflection = new ReflectionMethod('Jsonable', 'setAllObject');
+//$methodReflection->invoke(null, json_encode($arrayToJson));
